27a13071a0141775747372621478bfbca44c30c6
jjallaire <jj@rstudio.com>
2018-10-18 11:48:23 -0400

    attempt to address jankiness w/ empty image

--- src/components/Pack.vue
+++ src/components/Pack.vue
@@ -5,7 +5,7 @@
     <Card v-for="card in pack(player)" :key="card.key" 
           :player="player" :card="card" :drag_source="drag_source">
     </Card>
-    <span v-for="n in (15 - pack(player).length)" :key="n" 
+    <span v-for="n in (16 - pack(player).length)" :key="n" 
           class="card card-empty" draggable="false">
      <img src="images/card-empty.png" />
     </span>
@@ -47,14 +47,19 @@ export default {
   width: 11.5%;
   height: auto;
 }
+
+.pack .card-empty img {
+  user-select: none;
+  width: 11.5%;
+  height: auto;
+}
+
 @media only screen and (max-width: 1000px) {
-.pack .card img {
+.pack .card img,
+.pack .card-empty img {
   width: 10%;
 } 
 }
 
-.pack .card-empty img {
-  user-select: none;
-}
 </style>
 
