e427fadbda3e5df4cc7f8dfac7e979ca339a3ad9
jjallaire <jj@rstudio.com>
2018-10-26 21:31:00 -0400

    factor out chart data

--- src/components/ManaCurve.vue
+++ src/components/ManaCurve.vue
@@ -28,6 +28,9 @@ export default {
 
   watch: {
     cards: function(cards) {
+
+      let data = chartData(cards);
+      
       this.chart.update();
     }
   },
@@ -41,6 +44,36 @@ export default {
     this.chart = new Chartist.Line(this.$el, data);
   }
 }
+
+function chartData(cards) {
+  let creatures = new Array(6).fill(0);
+  let other = new Array(6).fill(0);
+  for (let i=0; i<cards.length;i++) {
+    let card = cards[i];
+    let isCreature = card.type_line.includes("Creature");
+    if (card.cmc <= 1)
+      if (isCreature)
+        creatures[0]++;
+      else
+        other[0]++;
+    else if (card.cmd >= 6)
+      if (isCreature)
+        creatures[5]++;
+      else
+        other[5]++;
+    else
+      if (isCreature)
+        creatures[card.cmc-1]++;
+      else
+        other[card.cmc-1]++;
+  }
+
+  return [
+    other,
+    creatures
+  ]
+}
+
 
 </script>
 
