2133ff726c6fb81a8e4f43f395ad5c77055e9baa
jjallaire <jj@rstudio.com>
2018-11-02 11:25:46 -0400

    tag formatting for deck.vue template

--- src/components/draft/deck/Deck.vue
+++ src/components/draft/deck/Deck.vue
@@ -3,15 +3,21 @@
 <template>
   <Panel caption="Deck" panel_class="mtgdraft-deck">
     <Pile :player="player" v-for="number in 5" 
-          :key="number-1" :caption="number + ''" :piles="piles" :number="number-1" drag_source="DRAG_SOURCE_DECK"></Pile>
-    <Pile :player="player" :key="5" caption="6+" :piles="piles" :number="5" drag_source="DRAG_SOURCE_DECK"></Pile>
+          :key="number-1" :caption="number + ''" :piles="piles" :number="number-1" 
+          drag_source="DRAG_SOURCE_DECK">
+    </Pile>
+    <Pile :player="player" :key="5" caption="6+" :piles="piles" :number="5" 
+          drag_source="DRAG_SOURCE_DECK">
+    </Pile>
     <Pile :player="player" :key="6" :caption="'Lands (' + deck_lands(this.player) + ')'"
           :piles="piles" :number="6" drag_source="DRAG_SOURCE_DECK">
       <Lands slot="controls" :player="player">
       </Lands>
     </Pile>
     <div class="mtgpile mtgpile-separator"></div>
-    <Pile :player="player" :key="7" caption="Sideboard" :piles="piles" :number="7" drag_source="DRAG_SOURCE_SIDEBOARD"></Pile>
+    <Pile :player="player" :key="7" caption="Sideboard" :piles="piles" :number="7" 
+          drag_source="DRAG_SOURCE_SIDEBOARD">
+    </Pile>
   </Panel>
 </template>
 
