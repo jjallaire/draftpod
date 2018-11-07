c4ccd2effb3d2adf0503835c72b0d97a93262de1
jjallaire <jj@rstudio.com>
2018-11-07 07:38:42 -0500

    move deckList function to getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -31,7 +31,7 @@ export default {
     let basic_lands = deck.basic_lands;
     return deck.piles[6].length + utils.sumValues(basic_lands);
   },
-  deck_list: (state) => (player) => utils.deckList(state.players[player].deck),
+  deck_list: (state) => (player) => deckList(state.players[player].deck),
   card_types: () => (cards) => {
     return {
       creatures: cards.filter(filters.creature).length,
@@ -40,4 +40,52 @@ export default {
     }
   },
   card_preview: (state) => (player) => state.players[player].card_preview,
+}
+
+function deckList(deck) {
+ 
+  // main deck
+  const card_name = (card) => card.name;
+  let main_deck = [].concat(
+    // cards
+    deck.piles.slice(0, 6).flat().map(card_name),
+    // special lands
+    deck.piles[6].map(card_name),
+    // basic lands
+    Array(deck.basic_lands.R).fill("Mountain"),
+    Array(deck.basic_lands.W).fill("Plains"),
+    Array(deck.basic_lands.U).fill("Island"),
+    Array(deck.basic_lands.B).fill("Swamp"),
+    Array(deck.basic_lands.G).fill("Forest")
+  );
+
+  // sideboard
+  let sideboard = deck.piles[7].map(card_name);
+    
+  // return deck list w/ main deck and sideboard
+  return asDeckList(main_deck) + 
+         '\n\n' +
+         asDeckList(sideboard);
+}
+
+
+// function to produce a text deck list
+function asDeckList(cards) {
+    
+  // conslidate duplicates
+  let deck_list = {};
+  cards
+    .slice()
+    .sort()
+    .map((name) => {
+      if (!deck_list.hasOwnProperty(name))
+        deck_list[name] = 0;
+      deck_list[name]++;
+    }
+  );
+
+  // return as list
+  return Object.keys(deck_list)
+    .map((name) => deck_list[name] + ' ' + name)
+    .join("\n");
 }
--- src/store/utils.js
+++ src/store/utils.js
@@ -34,53 +34,7 @@ export function initialState() {
   }
 }
 
-export function deckList(deck) {
- 
-  // main deck
-  const card_name = (card) => card.name;
-  let main_deck = [].concat(
-    // cards
-    deck.piles.slice(0, 6).flat().map(card_name),
-    // special lands
-    deck.piles[6].map(card_name),
-    // basic lands
-    Array(deck.basic_lands.R).fill("Mountain"),
-    Array(deck.basic_lands.W).fill("Plains"),
-    Array(deck.basic_lands.U).fill("Island"),
-    Array(deck.basic_lands.B).fill("Swamp"),
-    Array(deck.basic_lands.G).fill("Forest")
-  );
 
-  // sideboard
-  let sideboard = deck.piles[7].map(card_name);
-    
-  // return deck list w/ main deck and sideboard
-  return asDeckList(main_deck) + 
-         '\n\n' +
-         asDeckList(sideboard);
-}
-
-
-// function to produce a text deck list
-function asDeckList(cards) {
-    
-  // conslidate duplicates
-  let deck_list = {};
-  cards
-    .slice()
-    .sort()
-    .map((name) => {
-      if (!deck_list.hasOwnProperty(name))
-        deck_list[name] = 0;
-      deck_list[name]++;
-    }
-  );
-
-  // return as list
-  return Object.keys(deck_list)
-    .map((name) => deck_list[name] + ' ' + name)
-    .join("\n");
-}
 
 // sum all the values within an object
 export function sumValues(object) {
