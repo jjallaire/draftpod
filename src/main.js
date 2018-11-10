33a0ff8047312950a2970a856bac35beb09cee13
jjallaire <jj@rstudio.com>
2018-11-10 08:12:25 -0500

    update todo

--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ import './styles/slate.css'
 import router from './router'
 import store from './store'
 
-// awkwardness of startDraft
+// name of PICK_TIMER action
+
+// migration actions to mutations when possible
 
 // overly chatty PACK_TO_PICK (AI ends up calling it as well)
 
