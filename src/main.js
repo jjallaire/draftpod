c10a6ee36f8a4855713ce7c726f74636a07a40de
jjallaire <jj@rstudio.com>
2018-11-15 16:35:05 -0500

    update todo

--- src/main.js
+++ src/main.js
@@ -10,8 +10,6 @@ import store from './store'
 
 // TODO: card thumbnail for resume
 
-// TODO: color icons for no cards
-
 // TODO: ability to remove recent drafts?
 
 // TODO: implement separate piles for instants and sorceries in deck
