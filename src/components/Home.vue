db6b663dbe8c345c0a128a66d2b2337239a44cb2
jjallaire <jj@rstudio.com>
2018-11-17 08:12:20 -0500

    tweak home page colors

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,13 +20,13 @@ export default {
     
   <div class="container">
 
-  <div class="jumbotron">
+  <div class="jumbotron text-muted">
     <h1 class="display-3">Hello, world!</h1>
     <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
     <hr class="my-4">
     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
     <p class="lead">
-      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      <a class="btn btn-success btn-lg" href="#" role="button">Learn more</a>
     </p>
   </div>
 
@@ -38,6 +38,16 @@ export default {
 
 <style>
 
+.jumbotron {
+  background-image: linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%);
+  background-repeat: no-repeat;
+  background-color: transparent;
+  border: none;
+}
+
+.jumbotron .btn {
+  border: none;
+}
 
 </style>
 
