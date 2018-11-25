eecf725298d20c623ed4a371ef5987d63216b8d7
jjallaire <jj@rstudio.com>
2018-11-25 11:03:38 -0500

    add default vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+
+
+module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+    },
+  }
+}
