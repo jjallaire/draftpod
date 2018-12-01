d06419cc0ee3188469cac4f21408ea862a5823cb
jjallaire <jj@rstudio.com>
2018-12-01 06:47:10 -0500

    add some comments to draftbot

--- src/store/modules/draft/draftbot.js
+++ src/store/modules/draft/draftbot.js
@@ -1,28 +1,32 @@
 
-
+// pick a card given deck and pack
 export function pick(set_code, deck, pack) {
-  let top_rated = packRatings(deck, pack)[0];
-  return top_rated.card;
+  let ratings = cardRatings(deck, pack);
+  return ratings[0].card;
 }
 
 
-export function packRatings(deck, pack) {
+// determine the ratings for all cards in a pack
+export function cardRatings(deck, pack) {
 
-  // determine the top colors among playables (bonus for 3 colors
-  // before the fifth pick and consider 2 thereafter)
-  let num_colors = deck.length <= 5 ? 3 : 2;
+  // determine the colors used within the deck (consider up 
+  // to 3 colors before the 5th pick)
+  let num_colors = deck.length <= 4 ? 3 : 2;
   let deck_colors = deckColors(deck, num_colors);
 
-  // color bonus escalates over first 15 picks
+  // color bonus escalates gradually over first 15 picks
   const color_bonus_levels = [0.0, 0.0, 0.5, 0.5, 0.7, 
                               0.8, 1.0, 1.2, 1.4, 1.6,
                               1.6, 1.8, 1.8, 1.8, 2.0];
 
-  // synergy bonus levels
+  // synergy bonus escalates as cards w/ synergy are selected
   const synergy_bonus_levels = [0.0, 0.2, 0.5, 0.5, 0.5, 
                                 0.7, 0.7, 0.8, 0.9, 1.0];
 
+  // return ratings                               
   return pack
+
+    // transform card to card w/ rating info
     .map((card) => {
 
       // set baseline for ratings
@@ -36,21 +40,36 @@ export function packRatings(deck, pack) {
       if (card_colors.length === 0) {
         on_color = true;
       } else {
+        // how many of this card's colors match the deck colors?
         let matching_colors = card_colors.filter((color) =>
           deck_colors.indexOf(color) !== -1
         );
-        // match 2 out of 3 colors so draft AI picks 3-color cards
+        // on color if >= 66% of colors match (that way it's still 
+        // possible that we'll pick a 3-color card)
         on_color = (matching_colors.length / card_colors.length) >= 0.66;
       }
       
-      // if it's on-color then apply bonus
-      if (on_color)
-        color_bonus = color_bonus_levels[Math.min(deck.length, color_bonus_levels.length-1)];
+      // if it's on-color then apply bonus (bonus levels off after card 15)
+      if (on_color) {
+        color_bonus = color_bonus_levels[
+          Math.min(deck.length, color_bonus_levels.length-1)
+        ];
+      }
 
       // check for synergy
       if (card.synergy) {
-        let synergy_count = deck.filter((deck_card) => deck_card.synergy === card.synergy).length;
-        let synergy_bonus = synergy_bonus_levels[Math.min(synergy_count, synergy_bonus_levels.length-1)];
+        
+        // how many other cards in the deck share this synergy?
+        let synergy_count = deck.filter((deck_card) => { 
+          deck_card.synergy === card.synergy 
+        }).length;
+
+        // determine synergy bonus (escalates as more cards share the synergy)
+        let synergy_bonus = synergy_bonus_levels[
+          Math.min(synergy_count, synergy_bonus_levels.length-1)
+        ];
+
+        // apply the bonus if we have one
         if (synergy_bonus > 0) {
           synergy = {
             name: card.synergy,
@@ -59,6 +78,7 @@ export function packRatings(deck, pack) {
         }
       }
 
+      // return the card and the various components of the final adjusted rating
       return {
         card: card,
         base_rating: base_rating,
@@ -67,12 +87,15 @@ export function packRatings(deck, pack) {
         rating: base_rating + color_bonus + (synergy ? synergy.bonus : 0)
       }
     })
+
+    // order cards by rating
     .sort((a, b) => b.rating - a.rating);
 
 }
 
-
-function deckColors(deck, numColors) {
+// What are the colors of this deck? (multiply cards  of various colors by their
+// rating and then return the top numColors)
+export function deckColors(deck, numColors = 2) {
 
   let colorAffinity = deck.reduce((colors, card) => {
     card.colors.forEach((color) => {
