ef9308f7ac8e20431e11328bcbc81a8836dcc291
jjallaire <jj@rstudio.com>
2018-12-02 20:35:07 -0500

    re-pack options arguments for create draft

--- src/store/actions.js
+++ src/store/actions.js
@@ -13,7 +13,7 @@ export const CREATE_DRAFT = 'CREATE_DRAFT'
 
 export default {
 
-  [CREATE_DRAFT]( { commit }, { set_code, cardpool, pick_timer, pick_analysis, clear_table } ) {
+  [CREATE_DRAFT]( { commit }, options ) {
 
     // create a new draft module
     let draft_id = uuidv4();
@@ -23,15 +23,15 @@ export default {
     return new Promise((resolve) => {
 
       // download set data
-      axios.get('/sets/' + set_code + '/cards.json')
+      axios.get('/sets/' + options.set_code + '/cards.json')
         .then(response => {
 
           // all cards in the set
           let cardsInSet = response.data;
 
           // cardpool: either an explicit list or a generated cube
-          let [ common, uncommon, mythic, rare ] = cardpool.split('/').map(Number);
-          cardpool = set.cube(set_code, cardsInSet, {
+          let [ common, uncommon, mythic, rare ] = options.cardpool.split('/').map(Number);
+          let cardpool = set.cube(options.set_code, cardsInSet, {
             mythic: mythic,
             rare: rare,
             uncommon: uncommon,
@@ -40,12 +40,12 @@ export default {
           
           // initialize
           commit("drafts/" + draft_id + "/" + START_DRAFT, {
-            set_code,
+            set_code: options.set_code,
             cardpool: cardpool,
             options: {
-              pick_timer,
-              pick_analysis,
-              clear_table
+              pick_timer: options.pick_timer,
+              pick_analysis: options.pick_analysis,
+              clear_table: options.clear_table
             }
           });
 
