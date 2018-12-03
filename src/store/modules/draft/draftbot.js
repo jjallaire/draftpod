6eea941e292b48002d7df22dd7ce39709f49bc3b
jjallaire <jj@rstudio.com>
2018-12-03 15:40:36 -0500

    improve draftbot color selection (gold card matching, 2 not 3 colors during first 5 picks)

--- src/store/modules/draft/draftbot.js
+++ src/store/modules/draft/draftbot.js
@@ -9,15 +9,21 @@ export function pick(set_code, deck, pack) {
 // determine the ratings for all cards in a pack
 export function cardRatings(deck, pack) {
 
-  // determine the colors used within the deck (consider up 
-  // to 3 colors before the 5th pick)
-  let num_colors = deck.length <= 4 ? 3 : 2;
-  let deck_colors = deckColors(deck, num_colors);
+  // determine the colors used within the deck (2 colors with the
+  // highest overall power-level)
+  let deck_colors = deckColors(deck);
+
+  // determine what percent of a card's colors must 
+  // match for it to be considered 'on-color'. During Pack 1
+  // this will be 50% (to account for gold cards), during Packs
+  // 2-3 this will be 66% (so that bots will still draft
+  // 3-color cards)
+  let color_ratio = deck.length <= 14 ? 0.50 : 0.66;
 
   // color bonus escalates gradually over first 15 picks
-  const color_bonus_levels = [0.0, 0.0, 0.5, 0.5, 0.7, 
-                              0.8, 1.0, 1.2, 1.4, 1.6,
-                              1.6, 1.8, 1.8, 1.8, 2.0];
+  const color_bonus_levels = [0.0, 0.5, 0.6, 0.7, 0.8, 
+                              1.0, 1.1, 1.2, 1.3, 1.5,
+                              1.6, 1.7, 1.8, 1.9, 2.0];
 
   // synergy bonus escalates as cards w/ synergy are selected
   const synergy_bonus_levels = [0.0, 0.2, 0.5, 0.5, 0.5, 
@@ -38,15 +44,15 @@ export function cardRatings(deck, pack) {
       let on_color = false;
       let card_colors = card.colors;
       if (card_colors.length === 0) {
+        // artifacts are always on-color
         on_color = true;
       } else {
         // how many of this card's colors match the deck colors?
         let matching_colors = card_colors.filter((color) =>
           deck_colors.indexOf(color) !== -1
         );
-        // on color if >= 66% of colors match (that way it's still 
-        // possible that we'll pick a 3-color card)
-        on_color = (matching_colors.length / card_colors.length) >= 0.66;
+        // compute whether we are on-color
+        on_color = (matching_colors.length / card_colors.length) >= color_ratio;
       }
       
       // if it's on-color then apply bonus (bonus levels off after card 15)
