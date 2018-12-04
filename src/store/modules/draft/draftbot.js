239c8324d6319096e4656b5f7da71841b05d531a
jjallaire <jj@rstudio.com>
2018-12-04 09:40:33 -0500

    tweak synergy bonus level

--- src/store/modules/draft/draftbot.js
+++ src/store/modules/draft/draftbot.js
@@ -26,7 +26,7 @@ export function cardRatings(deck, pack) {
                               1.6, 1.7, 1.8, 2.0, 2.1];
 
   // synergy bonus escalates as cards w/ synergy are selected
-  const synergy_bonus_levels = [0.0, 0.2, 0.5, 0.5, 0.5, 
+  const synergy_bonus_levels = [0.0, 0.2, 0.5, 0.5, 0.6, 
                                 0.7, 0.7, 0.8, 0.9, 1.0];
 
   // return ratings                               
