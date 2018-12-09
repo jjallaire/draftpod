06cbb55f3092d80d8aa2826843b936d064f719e7
jjallaire <jj@rstudio.com>
2018-12-09 07:42:37 -0500

    some todo notes

--- src/main.js
+++ src/main.js
@@ -7,6 +7,26 @@ import router from './router'
 import { store } from './store'
 
 
+// TODO: firebase:
+
+//   - https://www.smashingmagazine.com/2018/04/vuejs-firebase-firestore/
+//    (describes how to setup security)
+
+//    Use onSnapshot to detect writes by other users
+
+//   - https://vuejsdevelopers.com/2017/10/16/vue-js-firestore/
+
+
+
+//   - https://medium.com/js-dojo/build-a-realtime-chat-app-with-vuejs-vuex-and-firestore-32d081668709
+//   - https://medium.com/@amenallah.hsoumi/building-a-progressive-quiz-app-with-vue-vuex-and-firestore-part-1-ce73c7ba695d
+
+// a library (but we should probably learn the low-level basics first):
+//    - https://mesqueeb.github.io/vuex-easy-firestore/
+// another library:
+//    - https://github.com/fiery-data/fiery-vuex
+
+
 // TODO: favicon
 
 // TODO: remove mana and set symbols?
