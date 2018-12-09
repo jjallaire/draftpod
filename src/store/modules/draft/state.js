1576165d2d29430b6c7b74adc21b595af0c609b4
jjallaire <jj@rstudio.com>
2018-12-09 07:58:21 -0500

    some comments on firestore

--- src/store/modules/draft/state.js
+++ src/store/modules/draft/state.js
@@ -1,6 +1,20 @@
 
 import { PICKS, DECK } from './constants'
 
+
+// - move start_time and options into table
+// - have a player_index outside of table
+// - track which of the players is a bot via a list of indexes inside the table
+// - sync the table to firestore
+// - players pass their pack into a zone explicitly (bots do it automatically, 
+//   but who polks the bot, perhaps the player that passed to them?)
+//      - for this would need to introduce a passing zone
+// - how to implement pick timer? (perhaps the host forces picks?). or perhaps
+//    there is a scheduled server-side function? maybe all clients can 
+//    be responsible for noticing the pick_timer and forcing the picks
+//    via a call to a firebase function
+// 
+
 export default function() {
   return {
 
