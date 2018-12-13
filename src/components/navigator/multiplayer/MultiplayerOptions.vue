4d88f68fb76871c93c718292771a7ed25e3e05e8
jjallaire <jj@rstudio.com>
2018-12-13 14:17:50 -0500

    make it easier to copy the join url

--- src/components/navigator/multiplayer/MultiplayerOptions.vue
+++ src/components/navigator/multiplayer/MultiplayerOptions.vue
@@ -2,13 +2,21 @@
 
 <script>
 
+import Vue       from 'vue'
+import Clipboard from 'v-clipboard'
+Vue.use(Clipboard)
+
+import ClipboardIcon from "vue-material-design-icons/ClipboardOutline.vue"
+
+import jquery from 'jquery'
+
 export default {
   name: 'MultiplayerOptions',
 
   props: {
     value: {
       type: Object,
-      required: true
+      required: true 
     }
   },
 
@@ -18,12 +26,35 @@ export default {
     }
   },
 
+  computed: {
+    join_url: function() {
+      return window.location.origin + this.$route.fullPath + 
+             this.inputVal.draft_id + "/join"
+    }
+  },
+
+  mounted() {
+    jquery('#copy-join-url-to-clipboard').tooltip({
+      title: 'URL copied!',
+      trigger: 'manual'
+    });
+  },
+
   methods: {
     onNameChanged(event) {
       this.inputVal.player_name = event.target.value.trim();
       this.$emit('input', this.inputVal);
+    },
+    onClipboardSuccess() {
+      let copy_url = jquery('#copy-join-url-to-clipboard');
+      copy_url.tooltip('show');
+      setTimeout(() => copy_url.tooltip('hide'), 1500);
     }
   },
+
+  components: {
+    ClipboardIcon
+  }
   
 }
 
@@ -38,15 +69,40 @@ export default {
   <label for="multiplayer-draft-name">Host player:</label>
   <input class="form-control" id="multiplayer-draft-name" placeholder="Enter your name" 
          :value="inputVal.player_name" @input="onNameChanged" ref="name"/>
-</div>
 
-{{ value.draft_id }}
+  <br/>
+  <p>Provide this URL to other players that wish to join the draft: </p>
+  
+  <span class="join-url">{{ join_url }} </span>
+
+  <a id="copy-join-url-to-clipboard"
+     v-clipboard="join_url"
+     v-clipboard:success="onClipboardSuccess"
+     data-toggle="tooltip" data-placement="top">
+    <ClipboardIcon title="Copy URL to clipboard"/>
+  </a>
+
+  </div>
 </div>
 
 </template>
 
 
 <style>
+
+.join-url {
+  color:#e9ecef ;
+}
+
+#copy-join-url-to-clipboard {
+  cursor: pointer;
+  margin-left: 8px;
+}
+
+#copy-join-url-to-clipboard:hover {
+  color: #eee;
+}
+
 
 </style>
 
