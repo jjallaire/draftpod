565efa969ee8388d8801e5f0ffc5c39db8b27ca8
jjallaire <jj@rstudio.com>
2018-12-14 07:46:33 -0500

    change title/caption

--- src/components/core/NavBar.vue
+++ src/components/core/NavBar.vue
@@ -8,7 +8,7 @@
     
     <slot>
       <span class="navbar-text">
-        Draft simulator for MTG
+        Virtual draft pod for Magic the Gathering<sup class="trademark">&trade;</sup>
       </span>
     </slot>
 
