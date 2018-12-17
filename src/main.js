078400d88ada0a8c9b3c7954f6d0df2531bdce85
jjallaire <jj@rstudio.com>
2018-12-17 13:27:48 -0500

    move todo

--- src/main.js
+++ src/main.js
@@ -51,29 +51,3 @@ function initApp(firebase_uid) {
   }).$mount('#app');  
 
 }
-
-// TODO: restore home and about
-
-// TODO: when writing / reading to firestore, "compress" the cards by just including
-// the multiverse id. could also be used for local storage?
-
-// TODO: favicon
-
-// TODO: remove mana and set symbols?
-
-// TODO: import Decked Builder YAML (.coll2)
-
-// TODO: sideboard:
-//    - automatic ordering?
-//    - drop targets to deck not big enough
-//    - trash/hide/second pile (pile which won't be in decklist) 
-
-// TODO: sane tablet experience
-
-// TODO: at larger deck sizes during building it's possible for a color
-// to be auto-assigned 0 mana even though there is a card in that color
-// (fraction is likely too small)
-
-// TODO: links:
-//   Ben Stark "Drafting the Hard Way"
-//   Reid Duke: "Draft Signals"
---
+++ tools/todo.md
@@ -0,0 +1,29 @@
+
+- restore home and about
+
+- when writing / reading to firestore, "compress" the cards by just including
+  the multiverse id. could also be used for local storage?
+
+- favicon
+
+- remove mana and set symbols?
+
+- import Decked Builder YAML (.coll2)
+
+- sideboard:
+    - automatic ordering?
+    - drop targets to deck not big enough
+    - trash/hide/second pile (pile which won't be in decklist) 
+
+- sane tablet experience
+
+- at larger deck sizes during building it's possible for a color
+  to be auto-assigned 0 mana even though there is a card in that color
+  (fraction is likely too small)
+
+
+Links:
+
+- Ben Stark "Drafting the Hard Way"
+- Reid Duke: "Draft Signals"
+
