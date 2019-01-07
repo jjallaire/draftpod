e5bee3ad00bd6897a667bdf4a25d72922c8168fc
jjallaire <jj@rstudio.com>
2019-01-07 13:03:39 -0500

    update storage version

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,7 @@ const debug = process.env.NODE_ENV !== 'production'
 Vue.use(Vuex)
 
 const vuexPersist = new VuexPersist({
-  key: 'draftpod-alpha2',
+  key: 'draftpod-alpha3',
   storage: window.localStorage
 });
 
