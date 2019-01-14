eb7dd07da8d25fa233db234429b2b75221ba79d1
jjallaire <jj@rstudio.com>
2019-01-14 08:58:10 -0500

    always show players popup

--- src/components/draft/table/TablePage.vue
+++ src/components/draft/table/TablePage.vue
@@ -350,7 +350,7 @@ export default {
       </span> 
     
       <ul class="navbar-nav">
-        <li v-if="!isPhone && options.multi_player || picks_complete" class="nav-item">
+        <li v-if="!isPhone" class="nav-item">
           <div class="dropdown">
             <a id="playersMenuLink" class="nav-link icon-link dropdown-toggle" title="Players" 
               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
