59e74ceee61237fabd90d6e5935e7c9734f56690
jjallaire <jj@rstudio.com>
2019-01-16 16:19:50 -0600

    bookmarkable guide sections

--- src/components/guide/GuidePage.vue
+++ src/components/guide/GuidePage.vue
@@ -10,14 +10,43 @@ import DraftCubeMd from './articles/draft-cube.md'
 import FAQMd from './articles/faq.md'
 import FeedbackMd from './articles/feedback.md'
 
+import jquery from 'jquery'
+
 export default {
   name: 'GuidePage',
+
+  mounted() {
+    let url = location.href.replace(/\/$/, "");
+
+    if (location.hash) {
+      const hash = url.split("#");
+      jquery('#v-pills-tab a[href="#'+hash[1]+'"]').tab("show");
+      url = location.href.replace(/\/#/, "#");
+      history.replaceState(null, null, url);
+      setTimeout(() => {
+        jquery(window).scrollTop(0);
+      }, 400);
+    } 
+    
+    jquery('a[data-toggle="pill"]').on("click", function() {
+      let newUrl;
+      const hash = jquery(this).attr("href");
+      if(hash == "#intro") {
+        newUrl = url.split("#")[0];
+      } else {
+        newUrl = url.split("#")[0] + hash;
+      }
+      newUrl += "/";
+      history.replaceState(null, null, newUrl);
+    });
+  },
 
   components: {
     NavBar, SiteFooter, 
     IntroMd, SinglePlayerMd, MultiPlayerMd, DraftCubeMd, FAQMd, FeedbackMd
   }
 }
+
 
 </script>
 
