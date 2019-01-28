47afb825fb636b3b39bcd5586706da9ea357e755
jjallaire <jj@rstudio.com>
2019-01-28 11:14:46 -0500

    improved join url copying for ios

--- src/components/navigator/multiplayer/MultiplayerOptions.vue
+++ src/components/navigator/multiplayer/MultiplayerOptions.vue
@@ -1,16 +1,12 @@
-
-
 <script>
-
-import Vue       from 'vue'
-import Clipboard from 'v-clipboard'
-Vue.use(Clipboard)
 
 import ClipboardIcon from "vue-material-design-icons/ClipboardOutline.vue"
 
 import MultiplayerPlayers from '@/components/core/MultiplayerPlayers.vue'
 
 import jquery from 'jquery'
+
+import * as log from '@/core/log'
 
 export default {
   name: 'MultiplayerOptions',
@@ -40,7 +36,10 @@ export default {
 
   },
 
+
   mounted() {
+
+    // setup copy tooltip
     jquery('#copy-join-url-to-clipboard').tooltip({
       title: 'URL copied!',
       trigger: 'manual'
@@ -51,13 +50,77 @@ export default {
     onNameChanged(event) {
       this.inputVal.player_name = event.target.value.trim();
       if (this.inputVal.player_name.length > 0)
-        this.$emit('input', this.inputVal);
+        this.$emit('joinUrl', this.inputVal);
     },
+   
+    onCopyJoinUrl(event) {
+
+      event.stopPropagation();
+      event.preventDefault();
+
+      // select the join url
+      this.selectJoinUrl(true);
+
+      try {
+
+        // perform the copy
+        document.execCommand('copy');
+
+        // remove selection
+        this.selectJoinUrl(false);
+    
+        // show success message
+        this.onClipboardSuccess();
+
+      } catch(error) {
+        log.logException(error, "onCopyJoinURL");
+      }
+    },
+
+    selectJoinUrl(select) {
+
+      // get reference to the join url textarea
+      let joinUrl = this.$refs["joinUrl"];
+      
+      // on iOS we need to do a special dance to make it selectable
+      let isiOSDevice = navigator.userAgent.match(/ipad|iphone/i);
+      if (isiOSDevice) {
+
+        let editable = joinUrl.contentEditable;
+        let readOnly = joinUrl.readOnly;
+
+        joinUrl.contentEditable = true;
+        joinUrl.readOnly = false;
+
+        var selection = window.getSelection();
+        selection.removeAllRanges();
+
+        if (select) {
+          let range = document.createRange();
+          range.selectNodeContents(joinUrl);
+          selection.addRange(range);
+          joinUrl.setSelectionRange(0, 999999);
+        } else {
+          joinUrl.setSelectionRange(0,0);
+        }
+
+        joinUrl.contentEditable = editable;
+        joinUrl.readOnly = readOnly;
+
+      // in most environments we can just call select()
+      } else {
+        if (select)
+          joinUrl.select();
+        else
+          window.getSelection().removeAllRanges();
+      }
+    },
+
     onClipboardSuccess() {
       let copy_url = jquery('#copy-join-url-to-clipboard');
       copy_url.tooltip('show');
       setTimeout(() => copy_url.tooltip('hide'), 1500);
-    }
+    },
   },
 
   components: {
@@ -81,17 +144,16 @@ export default {
 
   <MultiplayerPlayers :players="players" />
 
-  <p>Other players can join the draft using this URL:</p>
-  
-  <p>
-    <span class="join-url">{{ join_url }} </span>
-    <a id="copy-join-url-to-clipboard"
-      v-clipboard="join_url"
-      v-clipboard:success="onClipboardSuccess"
-      data-toggle="tooltip" data-placement="top">
-      <ClipboardIcon title="Copy URL to clipboard"/>
-    </a>
-  </p>
+  <div class="form-group">
+    <label for="multiplayer-join-url">
+      Other players can join using this URL: 
+      <a @click="onCopyJoinUrl" href="" id="copy-join-url-to-clipboard"
+         data-toggle="tooltip" data-placement="top">
+         <ClipboardIcon title="Copy URL to clipboard"/> Copy
+      </a>
+    </label>
+    <textarea id="multiplayer-join-url" readonly ref="joinUrl" :value="join_url" class="join-url" />
+  </div>
 
   <p>
     Click <strong>Start Draft</strong> once the other players have all joined and you are ready to begin the draft.
@@ -117,7 +179,7 @@ export default {
 }
 
 .join-url {
-  color: rgb(236,236,236); 
+  width: 100%;
 }
 
 #copy-join-url-to-clipboard {
@@ -126,8 +188,13 @@ export default {
 
 }
 
+#copy-join-url-to-clipboard {
+  color: #d9d9d9;
+}
+
 #copy-join-url-to-clipboard:hover {
   color: #eee;
+  text-decoration: none;
 }
 
 
