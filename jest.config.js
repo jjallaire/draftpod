4cf08a61de322e1e9b853b68aa7c7484869bb97d
jjallaire <jj@rstudio.com>
2019-01-30 20:28:24 -0500

    initial configuration for jest tests

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+
+// workaround for issue described here: https://github.com/vuejs/vue-cli/issues/1879#issuecomment-413869211
+process.env.VUE_CLI_BABEL_TARGET_NODE = true;
+process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true;
+
---
+++ src/components/core/__tests__/SetIcon.spec.js
@@ -0,0 +1,5 @@
+
+
+test('sanity test', () => {
+  expect(true).toBe(true)
+})
