d7299fd4de228e471c87b0309da78c8faa471836
jjallaire <jj@rstudio.com>
2019-02-04 08:18:54 -0500

    cite fan content policy

--- src/components/core/SiteFooter.vue
+++ src/components/core/SiteFooter.vue
@@ -13,8 +13,10 @@ export default {
 <template>
   <footer class="site-footer">
     <p class="small">
+      Draftpod is unofficial Fan Content permitted under the 
+      <a href="https://company.wizards.com/fancontentpolicy" target="_blank">Fan Content Policy</a>.
       The literal and graphical information presented on this site about Magic: The Gathering, 
-      including card images, the mana symbols, and Oracle text, is copyright Wizards of the Coast, LLC, 
+      including card images, is copyright &copy; Wizards of the Coast, LLC, 
       a subsidiary of Hasbro, Inc. This website is not produced by, endorsed by, supported by, 
       or affiliated with Wizards of the Coast.
     </p>
@@ -26,6 +28,11 @@ export default {
 .site-footer {
   margin-top: 70px;
   margin-bottom: 50px;
+}
+
+.site-footer a {
+  color: inherit;
+  text-decoration: underline;
 }
 
 </style>
