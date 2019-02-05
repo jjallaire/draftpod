fcf5f9f3bca00996d38259b1640fec6f68d84bde
jjallaire <jj@rstudio.com>
2019-02-05 13:04:52 -0500

    footer links

--- src/components/core/SiteFooter.vue
+++ src/components/core/SiteFooter.vue
@@ -19,18 +19,34 @@ export default {
     <p class="small">
       <CopyrightNoticeMd />
     </p>
+
+    <p class="text-center small">
+      <span class="footer-links pull-right">
+        <a href="/guide#welcome/">About</a>
+        <a href="/guide#faq/">FAQ</a>
+        <a href="/guide#privacy/">Privacy</a>
+        <a href="/guide#contact/">Contact</a>
+        <a href="https://github.com/jjallaire/draftpod">GitHub</a>
+      </span>
+    </p>
   </footer>
 </template>
 
 <style>
 
 .site-footer {
-  margin-top: 70px;
+  margin-top: 50px;
   margin-bottom: 50px;
 }
 
 .site-footer a {
-  color: inherit;
+  color: inherit !important;
+}
+
+.site-footer .footer-links a {
+  color: #bbb !important;
+  margin-left: 15px;
+  margin-right: 15px;
 }
 
 </style>
