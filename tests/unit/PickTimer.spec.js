ad3a44d38409e5719a5b103742c2c53acc8630a6
jjallaire <jj@rstudio.com>
2019-02-07 12:07:23 -0500

    test for pick timer

---
+++ tests/unit/PickTimer.spec.js
@@ -0,0 +1,32 @@
+
+
+import { mount } from '@vue/test-utils'
+import PickTimer from '../../src/components/draft/pick/PickTimer.vue'
+
+describe('PickTimer.vue', () => {
+
+  function pickTimerOptions(secondsRemaining) {
+    return {
+      propsData: { pick_end_time: new Date().getTime() + (1000 * secondsRemaining) },
+      provide: {
+        pickTimerPick: null
+      }
+    }
+  }
+
+  test('displays time remaining', () => {
+    const wrapper = mount(PickTimer, pickTimerOptions(20));
+    expect(wrapper.text()).toContain('0:');
+    expect(wrapper.classes()).toContain('badge-transparent');
+  });
+
+  test('displays red background', () => {
+    const wrapper = mount(PickTimer, pickTimerOptions(9));
+    expect(wrapper.classes()).toContain('badge-danger');
+  });
+
+});
+
+
+
+
