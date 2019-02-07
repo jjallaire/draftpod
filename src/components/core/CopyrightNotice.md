b526682bb74532494f001b577af902a6cce1df53
jjallaire <jj@rstudio.com>
2019-02-07 08:15:00 -0500

    don't use markdown for site footer (interering with tests)

--- src/components/core/CopyrightNotice.md
+++
@@ -1,8 +0,0 @@
-
-
-Draftpod is unofficial Fan Content permitted under the Wizards [Fan Content Policy](https://company.wizards.com/fancontentpolicy). The literal and graphical information 
-presented on this site about Magic: The Gathering, including card images, is copyright &copy; 
-Wizards of the Coast, LLC, a subsidiary of Hasbro, Inc. This website is not produced by, 
-endorsed by, supported by, or affiliated with Wizards of the Coast.
-
-Card ratings are derived from the P1P1 pick order lists published by [Draftaholics Anonymous](https://www.draftaholicsanonymous.com). Card data is drawn from the [Scryfall API](https://scryfall.com/docs/api). The Draftpod logo was designed by [Faithtoken](http://www.faithtoken.com/) and is used via the [CC BY 3.0](https://creativecommons.org/licenses/by/3.0/) license. Material Design UI icons are &copy; 2014 Austin Andrews and &copy; 2014 Google.
--- src/components/core/SiteFooter.vue
+++ src/components/core/SiteFooter.vue
@@ -2,14 +2,9 @@
 
 <script>
 
-import CopyrightNoticeMd from './CopyrightNotice.md'
 
 export default {
   name: 'SiteFooter',
-
-  components: {
-    CopyrightNoticeMd
-  }
 }
 
 </script>
@@ -17,7 +12,19 @@ export default {
 <template>
   <footer class="site-footer text-muted">
     <p class="small">
-      <CopyrightNoticeMd />
+      Draftpod is unofficial Fan Content permitted under the Wizards 
+      <a href="https://company.wizards.com/fancontentpolicy">Fan Content Policy</a>(https://company.wizards.com/fancontentpolicy). 
+      The literal and graphical information  presented on this site about Magic: The Gathering, including card images, is copyright
+      &copy; Wizards of the Coast, LLC, a subsidiary of Hasbro, Inc. This website is not produced by, endorsed by, supported by, 
+      or affiliated with Wizards of the Coast.
+    </p>
+    <p class="small">
+      Card ratings are derived from the P1P1 pick order lists published by
+      <a href="https://www.draftaholicsanonymous.com">Draftaholics Anonymous</a>. 
+      Card data is drawn from the <a href="https://scryfall.com/docs/api">Scryfall API</a>. 
+      The Draftpod logo was designed by <a href="http://www.faithtoken.com/">Faithtoken</a> 
+      and is used via the <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a> license. 
+      Material Design UI icons are &copy; 2014 Austin Andrews and &copy; 2014 Google.
     </p>
 
     <p class="small">
