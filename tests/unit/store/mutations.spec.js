5029d522f7b1aed9dddfecd0e9fc0ba35660267b
jjallaire <jj@rstudio.com>
2019-02-09 14:41:33 -0500

    tests for update preferences

--- tests/unit/store/mutations.spec.js
+++ tests/unit/store/mutations.spec.js
@@ -1,7 +1,7 @@
 
 
-
-import { SET_PLAYER_INFO } from '@/store/mutations'
+import { CARDPOOL } from '@/store/constants'
+import { SET_PLAYER_INFO, UPDATE_PREFERENCES } from '@/store/mutations'
 
 import { testStore } from '../../util/test-store'
 
@@ -22,6 +22,24 @@ describe('Store Mutations', () => {
 
     expect(store.getters.player.id).toBe(newId);
     expect(store.getters.player.name).toBe(newName);
+
+  });
+
+  test('update preferences', () => {
+
+    let newSet = 'dom';
+    let newCardpool = CARDPOOL.CUBE + '3/2/1/1';
+    store.commit(UPDATE_PREFERENCES, {
+      set_code: newSet,
+      cardpool: newCardpool,
+      pick_timer: true,
+      pick_ratings: true
+    });
+
+    expect(store.state.preferences.set_code).toBe(newSet);
+    expect(store.state.preferences.sets.dom.cardpool).toBe(newCardpool);
+    expect(store.state.preferences.pick_timer).toBe(true);
+    expect(store.state.preferences.pick_ratings).toBe(true);
 
   });
   
