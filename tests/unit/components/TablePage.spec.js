8da8bb4f234b07c2f89e7cd3ad5edd58f0b1f06e
jjallaire <jj@rstudio.com>
2019-02-09 16:19:26 -0500

    test to make sure that the table page can load a draft

---
+++ tests/unit/components/TablePage.spec.js
@@ -0,0 +1,39 @@
+
+import { mount } from '@vue/test-utils'
+import TablePage from '../../../src/components/draft/table/TablePage.vue'
+
+import { testStore } from '../../util/test-store'
+
+import { RouterLinkStub } from '@vue/test-utils'
+
+import flushPromises from 'flush-promises'
+
+describe('TablePage.vue', () => {
+
+  test('draft table displays', () => {
+
+    // mount the page
+    const wrapper = mount(TablePage, {
+      sync: false,
+      propsData: { draft_id: "2me2Q4mvbpQEJVeMKkuk3y" },
+      mocks: {
+        $store: testStore()
+      },
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    });
+
+    return flushPromises().then(() => {
+
+      // do a nominal check for some content (this test is really intended to
+      // see whether the page renders at all)
+      const deckPiles = wrapper.findAll('.pick-list .pile');
+      expect(deckPiles).toHaveLength(9);
+
+    });
+
+    
+  });
+
+});
