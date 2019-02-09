5066b19e53a9e62770963e5f1bbb62f2fe4b3ce7
jjallaire <jj@rstudio.com>
2019-02-09 14:19:12 -0500

    more tests for getters

--- tests/unit/store/getters.spec.js
+++ tests/unit/store/getters.spec.js
@@ -26,4 +26,24 @@ describe('Store Getters', () => {
     expect(draft.id).toBe('2me2Q4mvbpQEJVeMKkuk3y');
   });
 
+  test('orphaned drafts', () => {
+    let orphaned = store.getters.orphaned_drafts;
+    expect(orphaned.length).toBe(0);
+  });
+
+  test('cardpools', () => {
+
+    // enumeration
+    expect(store.getters.cardpools.length).toBe(1);
+    
+    // select a cardpool
+    let cardpool = store.getters.cardpool('rna', 'RNA Cube');
+    expect(cardpool.name).toBe('RNA Cube');
+
+    // get cardpool options
+    let options = store.getters.cardpool_options('rna');
+    expect(options.custom.length).toBe(1);
+
+  });
+
 });
