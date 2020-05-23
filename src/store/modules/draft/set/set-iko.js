d85a9fa1cf58cb83916d609b7cad8a54e33d3f6c
JJ Allaire <jj@rstudio.com>
2020-05-23 13:01:02 -0400

    handle dual lands in iko as we do in m20

--- src/store/modules/draft/set/set-iko.js
+++ src/store/modules/draft/set/set-iko.js
@@ -2,28 +2,47 @@
 
 import * as cube from './cube'
 import * as booster from './booster'
-
+import * as filters from '../card-filters'
 
 export default {
 
   name: "Ikoria",
 
-  pack_cards: () => 14,
+  pack_cards: () => 15,
 
   cube: cube.build,
+
+  cardpool_basics: [482535,482538,482541,482544,482547],
 
   booster(selectCards) {
 
     let cards = [].concat(
       selectCards(booster.packRareSlot, 1),
       selectCards(booster.uncommon, 3),
-      selectCards(booster.common, 10)
+      selectCards(commonNotDualLand, 10)
     );
 
-    return cards;
+    if (Math.random() <= (5/12)) {
+      // fallback to basic if there aren't enough dual lands
+      const dualLand = [ikoDualLand, filters.basicLand];
+      return cards.concat(selectCards(dualLand, 1));
+    } else {
+      return cards.concat(selectCards(filters.basicLand, 1));
+    }
   },
 
 }
+
+export function ikoDualLand(card) {
+  const DUAL_LANDS =  ["Tranquil Cove", "Scoured Barrens", "Dismal Backwater",
+                       "Swiftwater Cliffs", "Bloodfell Caves", "Jungle Hollow",
+                       "Rugged Highlands", "Wind-Scarred Crag", "Blossoming Sands",
+                       "Thornwood Falls", "Evolving Wilds"];
+  return DUAL_LANDS.indexOf(card.name) >= 0;
+}
+
+const commonNotDualLand = filters.join(filters.common, card => !ikoDualLand(card));
+
 
 
 
