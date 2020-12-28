6b993c86cae96500418755f3702fa3df5bde1918
JJ Allaire <jj@rstudio.com>
2020-12-28 11:06:00 -0500

    update title and description

--- src/site.js
+++ src/site.js
@@ -1,7 +1,7 @@
 
 
 module.exports = {
-  title: process.env.VUE_APP_SITE_TITLE || 'Draftpod',
+  title: process.env.VUE_APP_SITE_TITLE || 'Draftpod: MTG Draft and Sealed Simulator',
   url: process.env.VUE_APP_SITE_URL || 'https://draftpod.org/',
   twitter: process.env.VUE_APP_SITE_TWITTER || 'https://twitter.com/DraftpodMTG'
 }
