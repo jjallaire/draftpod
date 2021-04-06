7559e91cfbc8887da804177eb50ba22cd60d7ca2
JJ Allaire <jj@rstudio.com>
2021-04-06 19:32:32 -0400

    default to khm until stx release

--- src/store/index.js
+++ src/store/index.js
@@ -49,7 +49,7 @@ export function initializeStore() {
       name: null
     },
     preferences: {
-      set_code: 'stx',
+      set_code: 'khm',
       format: 'draft',
       pick_timer: false,
       pick_ratings: false,
